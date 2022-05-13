33676a5450cee7855335dbcaad241ec106ca7e2b
Henrique <up201805000@up.pt>
2022-05-13 15:13:48 +0100

    index correction

--- index.php
+++ index.php
@@ -1,26 +1,47 @@
 <?php 
-
   $db = new PDO('sqlite:database/database.db');
 
   $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $stmt = $db->prepare('SELECT restaurant.id, reviews.rate, dish.name FROM restaurant
+  $stmt = $db->prepare('SELECT restaurant.* FROM restaurant
    LEFT JOIN reviews ON reviews.restaurant_id=restaurant.id 
    LEFT JOIN dish ON dish.restaurant_id=restaurant.id
   GROUP BY restaurant.id');
 
   $stmt->execute();
-
-  require_once('templates/elements.php');
-
-  generateHeader();
+  $restaurants = $stmt->fetchAll();
 ?>
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Title</title>
+    <meta charset="UTF-8">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/layout.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="index.php">
+            <img src="logo.jpeg" alt="logo">
+        </a>
+        <form id="search-bar" action="search-results.php" method="get">
+            <input type="text" name="search-bar" placeholder="Restaurant, city, ...">
+        </form>
+        <div id="signup">
+            <a href="register.php">Register</a>
+            <a href="login.php">Login</a>
+      </div>
+    </header>
 
     <section id="restaurants-examples">
-
+    <?php foreach( $restaurants as $restaurant) { ?>
+      <article>
+        <header>
+          <h1><a href="item.php?id=<?= $restaurant['id'];?>"> <?=  $restaurant['name']; ?> </a></h1>
+        </header>
+        <?php }?>
     </section>
 </body>
 </html>
