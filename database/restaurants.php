c7c237bf585c331048d1e0f18786b581e1b16c50
Henrique <up201805000@up.pt>
2022-05-30 15:08:55 +0100

    pagina do restaurant, só mais uma

--- database/restaurants.php
+++ database/restaurants.php
@@ -3,14 +3,21 @@
 const ALL_RESTAURANTS_Q= 'SELECT restaurant.* FROM restaurant
 GROUP BY restaurant.id';
 
-const REVIEWS_Q='SELECT reviews.* FROM reviews
+const REVIEWS_Q='SELECT reviews.rate FROM reviews
 WHERE restaurant_id = ?
 GROUP BY reviews.id';
 
+const RESTAURANT='SELECT restaurant.* FROM restaurant WHERE id=?';
 function getAllRestaurants(PDO $db){
     $stmt = $db->prepare(ALL_RESTAURANTS_Q);
     $stmt->execute();
     return $stmt->fetchAll();
+}
+
+function getRestaurant(PDO $db, int $id){
+    $stmt = $db->prepare(RESTAURANT);
+    $stmt->execute(array($id));
+    return $stmt->fetch();
 }
 
 
@@ -43,5 +50,14 @@ function getRestaurantRate(PDO $db, int $id){
     return $stmt->fetchAll();
 }
 
+function countReviews(PDO $db, int $id){
+    $reviews = getRestaurantRate($db, $id);
+    $storeRev=[0,0,0,0,0,0];
+    
+    foreach ( $reviews as $review){
+        $storeRev[$review['rate']]=$storeRev[$review['rate']]+1;
+      } 
+      return $storeRev;
+}
 
 ?>
--- restaurant.php
+++ restaurant.php
@@ -1,12 +1,26 @@
 <?php
+require_once('./database/connection.php');
+require_once('./database/restaurants.php');
+
 require_once('./templates/elements.tpl.php');
+require_once('./templates/restaurant.tpl.php');
+
+$id=intval($_GET['id']);
+$db = getDatabaseConnection();
+$restaurant = getRestaurant($db, $id);
+$storeRev= countReviews($db, $id);
+
 drawHeader();
 ?>
 
-<!DOCTYPE html>
-
-<div id="1">
-    <p>First Restaurant</p>
+<div>
+    <p><?= $restaurant['name']?> </p>
+    <p><?php 
+    for($i=0; $i<sizeof($storeRev); $i+=1){ //para fazer tabela com nr de reviews
+        echo $storeRev[$i]. " ";
+    }
+    ?>
+    </p>
 </div>
 
 <?php drawFooter(); ?>
