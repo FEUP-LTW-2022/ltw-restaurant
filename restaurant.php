36a4224f7201e5ae73c3b58fbe7f6014a10619d1
Henrique <up201805000@up.pt>
2022-06-03 00:20:50 +0100

    restaurant page draft

--- restaurant.php
+++ restaurant.php
@@ -20,22 +20,32 @@ $dishes = getDishes($db,$id);
 
 $category= getRestaurantCategory($db, $restaurant['category']);
 
-
-
 drawHeader();
 
 //drawDishesList($dishes);
 ?>
 
 <div class="restaurant-page">
-    <a id="header-category"><?= $category['name']?></a> <!-- href -> search de todos restaurantes da categoria -->
-    <h1><?= $restaurant['name']?> </h1>
-    <section class="dishes-list">
+    <span><a id="header-category"><?= $category['name']  ?></a> </span> <!-- href -> search de todos restaurantes da categoria -->
+    <div class="res-page-name">
+        <span id="r_name"><b><?= $restaurant['name']." - ".$restaurant['city']?> </b></span>
+        <div>
+            <span id="r_rate"><?=$avgRev?></span>
+            <span>/5</span>
+        </div>
+    </div>
+    <span id="maps"> <a href="https://www.google.com/maps/search/?api=1&query=<?=$restaurant['address']?>"><u><?= $restaurant['address']?></u></a></span>
+    <p><img src="<?=$restaurant['photo']?>" alt="restaurant photo" style="  width: 800px; height: 300px;"></p>
+
+
+
+    <section class="menu">
+        <h2>Menu</h2>
         <?php foreach( $dishes as $dish) {?>
             <article>
-                <div id="dish-info">
-                    <div id="dish-name"><b><?=$dish['name']?> </b> </div>
-                </div>
+                <span id="dish-name"><?=$dish['name']?></span>
+                <hr id="menuHr">
+                <span id="dish-price"><?=$dish['price']?>&euro;</span>
             </article>
             <?php }?>
     </section>
