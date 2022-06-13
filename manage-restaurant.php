f62fb6b5617b80627997c19be34c77bd15d30276
BlackAlbino17 <blackalbino17@gmail.com>
2022-06-13 14:21:02 +0100

    created manage-restaurant.php layout

--- manage-restaurant.php
+++ manage-restaurant.php
@@ -1,2 +1,44 @@
 <?php
 session_start();
+
+include_once('database/connection.php');
+include_once('database/account.class.php');
+require_once('templates/elements.tpl.php');
+
+drawHeader();
+?>
+
+<div class="manage-restaurant">
+    <form method="post" action="manage-restaurant.php">
+        <h1>Edit restaurant</h1>
+        <label for="name">
+            <b>Name</b>
+            <input name="name" type="text" placeholder="Restaurant name .."  required>
+        </label>
+
+        <label for="category"><b>Category</b>
+            <select name="category">
+                <option value="steakhouse">Steakhouse</option>
+                <option value="fastfood">Fast Food</option>
+                <option value="italian">Italian</option>
+                <option value="pizzeria">Pizzeria</option>
+                <option value="mediterranean">Mediterranean</option>
+            </select>
+        </label>
+
+        <label for="address">
+            <b>Address</b>
+            <input name="address" type="text" placeholder="Restaurant address .."  required>
+        </label>
+
+        <label for="openHours"><b>Open hours</b>
+            <input name="openHour" type="time" required>
+            <input name="closeHour" type="time" required>
+        </label>
+        <br>
+
+        <button type="submit">Update</button>
+    </form>
+</div>
+
+<?php drawFooter(); ?>
