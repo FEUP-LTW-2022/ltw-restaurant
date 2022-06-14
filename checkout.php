f3d54a2df851ac5689a23adf1d64a12e62a6b318
Henrique <up201805000@up.pt>
2022-06-14 16:49:08 +0100

    fix

--- checkout.php
+++ checkout.php
@@ -10,10 +10,10 @@ drawHeader();?>
                 <form action="">
                     <div class="col-50">
                         <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b>4</b></span></h4>
-                        <p><span href="#">Product 1</span> <span class="price">15</span></p>
-                        <p><span href="#">Product</span> <span class="price">5</span></p>
-                        <p><span href="#">Product</span> <span class="price">8</span></p>
-                        <p><span href="#">Product</span> <span class="price">2</span></p>
+                        <p><span>Product 1</span> <span class="price">15</span></p>
+                        <p><span>Product</span> <span class="price">5</span></p>
+                        <p><span>Product</span> <span class="price">8</span></p>
+                        <p><span>Product</span> <span class="price">2</span></p>
                         <hr>
                         <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
 
